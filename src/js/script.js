eae6281aa14b7ff2ad6b2be0a2f6085269d10eab
ryu.ryu <ryumiya51+53@gmail.com>
2024-04-12 03:58:51 +0900

    ヘッダーSP版ドロワーメニュー途中まで 24.04.12

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,15 @@
 
 jQuery(function ($) { // この中であればWordpressでも「$」が使用可能になる
-
+  
+  //ドロワーメニュー
+  $(".js-hamburger, .js-sp-nav").click(function () {  // .js-sp-navを追加しXマーク以外がクリックされてもドロワーメニューがfadeOutするようにした
+    if($(".js-hamburger").hasClass('is-active')) {
+      $(".js-hamburger").removeClass("is-active");
+      $(".js-sp-nav").fadeOut(300);
+    } else {
+      $(".js-hamburger").addClass("is-active");
+      $(".js-sp-nav").fadeIn(300);
+    }
+  });
 
 });
