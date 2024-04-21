6254e8ab92830da87611f9e1fe903be49c75e189
ryu.ryu <ryumiya51+53@gmail.com>
2024-04-21 19:55:56 +0900

    mv、campaign完了 24.04.21

--- src/js/script.js
+++ src/js/script.js
@@ -1,9 +1,7 @@
-
-jQuery(function ($) { // この中であればWordpressでも「$」が使用可能になる
-  
+jQuery(function ($) {
   //ドロワーメニュー
-  $(".js-hamburger, .js-sp-nav").click(function () {  // .js-sp-navを追加しXマーク以外がクリックされてもドロワーメニューがfadeOutするようにした
-    if($(".js-hamburger").hasClass('is-active')) {
+  $(".js-hamburger, .js-sp-nav").click(function () {
+    if ($(".js-hamburger").hasClass('is-active')) {
       $(".js-hamburger").removeClass("is-active");
       $(".js-sp-nav").fadeOut(300);
     } else {
@@ -22,4 +20,81 @@ jQuery(function ($) { // この中であればWordpressでも「$」が使用可
     $('.js-sp-nav').fadeOut(300);
   });
 
+  // mvスワイパー
+  const mv_swiper = new Swiper('.js-mv-swiper', {
+    // 以下の項目はグーグルで「swiper オプション」で調べられる
+    loop: true,
+    effect: 'fade',
+    speed: 3000, // スライド（ここではフェイド）が変わるスピード
+    allowTouchMove: false, // 3秒(delay: 3000)たつ前にマウスでカチャカチャなぞる(?)ことによって次のスライドへ移るのをさせないようにする（これがないとクリックで自分でスライドできてしまう）
+    autoplay: {
+      delay: 3000, // 3秒後にスライドが変わっていく
+    },
+  });
+
+  // campaignスワイパー
+  const campaign_swiper = new Swiper('.js-campaign-swiper', {
+    slidesPerView: 'auto',
+    loop: true,
+    speed: 1000,
+    spaceBetween: 24,
+    // loopedSlides: 4,
+    breakpoints: {
+      768: {
+        spaceBetween: 40
+      }
+    },
+    autoplay: {
+      speed: 1000,
+      disableOnInteraction: false
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev"
+    }
+  });
+
+  // スクロールしながらページトップへ戻るボタン
+  let topBtn = $('.js-to-top');
+  topBtn.hide();
+
+  // ボタンの表示設定
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 70) {
+      // 指定px以上のスクロールでボタンを表示
+      topBtn.fadeIn();
+    } else {
+      // 画面が指定pxより上ならボタンを非表示
+      topBtn.fadeOut();
+    }
+  });
+
+  // ボタンをクリックしたらスクロールして上に戻る
+  topBtn.click(function () {
+    $('body,html').animate({
+      scrollTop: 0
+    }, 300, 'swing');
+    return false;
+  });
+
+  // Contactセクションの右下でボタンが止まる
+  $('.js-to-top').hide();
+  $(window).on('scroll', function () {
+    let documentHeight = $(document).height(); // ドキュメント全体の高さ
+    let wHeight = $(window).height(); // ブラウザの表示領域の高さ
+    let scrollAmount = $(window).scrollTop(); // スクロールした距離
+    let footerHeight = $('.js-footer').innerHeight(); // フッターの高さ(padding含む)
+    if (documentHeight - (wHeight + scrollAmount) <= footerHeight) {
+      // ページトップへ戻るボタンがフッターの直前に来たら、positionプロパティの値をfixedからabsoluteに変更する
+      $('.js-to-top').css({
+        position: 'absolute',
+        bottom: footerHeight + 16
+      });
+    } else {
+      $('.js-to-top').css({
+        position: 'fixed',
+        bottom: '16px'
+      });
+    }
+  });
 });
